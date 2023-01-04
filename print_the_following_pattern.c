6fc70b2024d9db9ca8c5b3338b4883261f28fce5
Mansoor Ahmad Shaik <119730233+Mansoor786786@users.noreply.github.com>
2023-01-04 19:51:06 +0530

    #include<stdio.h> int main() {     int i,j,n,t;     scanf("%d",&n);     for(i=1,t=(64+i);i<=n;i++,t++)     {         for(j=1;j<=n;j++)         {             printf("%c ",t);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int i,j,n,t;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1,t=(64+i);i<=n;i++,t++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            printf("%c ",t);
         }
         printf("
 ");

917f17a68d78fbb9bb7d3c3cef53391b8b96c09f
Mansoor Ahmad Shaik <119730233+Mansoor786786@users.noreply.github.com>
2023-01-07 22:23:30 +0530

    #include<stdio.h> int main() {     int n,l;     while(n!=-1)     {     scanf("%d",&n);     if(n==-1)     {         break;     }     printf("%d ",n*n);     } }

--- Infinite.c
+++ Infinite.c
@@ -4,12 +4,12 @@ int main()
     int n,l;
     while(n!=-1)
     {
-        scanf("%d",&n);
-        if(n==-1)
-        {
-            break;
-        }
-        printf("%d
+    scanf("%d",&n);
+    if(n==-1)
+    {
+        break;
+    }
+    printf("%d
 ",n*n);
     }
 }
